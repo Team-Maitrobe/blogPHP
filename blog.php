debb7da6ab397453b81d6592c36835021d8f2801
RAYNAUD OCTAVE <octave.raynaud@etu.univ-lyon1.fr>
2024-10-11 10:54:20 +0200

    first commit, adding blog.php file and connexion.php file

---
+++ blog.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlogPHP</title>
+</head>
+<body>
+    <h1>Bienvenue sur le blog</h1>
+    <p>blog.exe a arrêté de fonctionner</p>
+
+    <p>Connectez vous en cliquant ici : <a href="./connexion.php">Connectez-vous !</a></p>
+</body>
+</html>
---
+++ connexion.php
@@ -0,0 +1,33 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BlogPHP</title>
+    </head>
+    <body>
+        <header>
+            header
+        </header>
+        <main>
+            <h1>Bienvenue sur le blog</h1>
+            <p>blog.exe a arrêté de fonctionner</p>
+
+            <form method="POST">
+                <label for="pseudo">Saisir votre pseudo ou adresse email :</label>
+                <input type="text" name="pseudo" />
+
+                <label for="pseudo">Saisir votre mot de passe :</label>
+                <input type="text" name="mdp" />
+                
+                <input type="submit">
+            </form>
+        </main>
+        <footer>
+            footer
+        </footer>
+    </body>
+</html>
