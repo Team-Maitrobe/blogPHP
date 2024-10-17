fc1fc166d9730b0e84248f01801ef9f28ce54412
dotzito <raynaudoctave0@gmail.com>
2024-10-17 13:58:38 +0200

    Ajout du lien avec la BD + corrections mineures

--- poster.php
+++ poster.php
@@ -1,4 +1,16 @@
 <?php
+if(!empty($_POST['submit'])) {
+    $titre = $_GET['titre'];
+    $typecommentaire = $_GET['typecommentaire'];
+    $commentaire = $_GET['commentaire'];
+
+    $sql = 'INSERT INTO POST(titre_post, type_post, contenu_post) 
+            VALUES(:titre, :typecommentaire, :contenu_post)';
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(["titre" => $titre, "typecommentaire" => $typecommentaire, "commentaire" => $commentaire]);}
+
+
 ?>
 
 <?php include 'header.php'; ?>
@@ -7,15 +19,15 @@
             <h1>/**Nom du restaurant**/</h1>
         <div class="boite-bleue">          
             <form method="POST">
-                <input type="text" name="titre" placeholder="Titre" />
+                <input type="text" name="titre" placeholder="Titre" required/>
                 
-                <input type="radio" name="typecomentaire" value="avis"/>
+                <input type="radio" name="typecommentaire" value="avis"/>
                 <label for="avis">Avis</label>
 
-                <input type="radio" name="typecomentaire" value="question"/>
+                <input type="radio" name="typecommentaire" value="question"/>
                 <label for="question">Question</label>
 
-                <input type="text" name="commentaire" placeholder="Ecrire ici" />
+                <input type="text" name="commentaire" placeholder="Ecrire ici" required/>
 
                 <input type="submit">
             </form>
